a7e32b1632b4284194598fb88cf788b8ca7548f8
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-11 12:00:28 -0700

    feat: Create operator component
    In this commit I created the operator component which will be used to
    render individual operator(s) for our calculator app. A props of which
    will represent the buttonCharacter from the parent component is passed
    into the button component and is used for creating the Operator component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import "./App.css";
 function App() {
   const [numberState, setNumberState] = useState(numbers);
   console.log('this num state', numberState);
+  console.log('this operator is', operators);
+  console.log('that special is', specials);
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
