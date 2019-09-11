77c1b1e32acacdc85a52d6c2a68d1bd26c74d6b7
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-11 13:19:14 -0700

    feat: Create special button component
    In this commit, I created special button component. this component is
    used for creating the special button items.

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const { specialBtn } = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      { specialBtn }
     </>
   );
 };
