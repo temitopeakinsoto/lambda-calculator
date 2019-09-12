d782cf53b103808be027366beec04010f543a5a1
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-12 01:39:05 -0700

    Refactor: clean up all unused variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Logo from "./components/DisplayComponents/Logo";
-import { numbers, operators, specials } from "../src/data";
+import { numbers } from "../src/data";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
@@ -8,11 +8,7 @@ import Display from "./components/DisplayComponents/Display";
 import "./App.css";
 
 function App() {
-  const [numberState, setNumberState] = useState(numbers);
-  // console.log('this num state', numberState);
-  // console.log('this operator is', operators);
-  // console.log('that special is', specials);
-  
+ 
   const [display, setDisplay] = useState(0);
 
   return (
